abbd3293f35510590fdc30a160f9908ae240e5ca
ethanwilkins <ethanforrestwilkins@gmail.com>
2019-12-19 23:18:14 -0500

    old_sprockets

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module Simplr
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
     # for loading custom libraries
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
