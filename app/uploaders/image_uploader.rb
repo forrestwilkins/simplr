ac4de866d62340e851efdb297e50dd54b2d782d0
ethanwilkins <ethanforrestwilkins@gmail.com>
2019-12-23 13:22:03 -0500

    prod_fix_2

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,7 +20,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.model_name.singular_route_key}/#{mounted_as}/#{model.id}"
   end
 
   process :fix_exif_rotation
