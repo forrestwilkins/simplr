960d3444987ef20ad020ec5a6da2acee1c830daa
ethanwilkins <ethanforrestwilkins@gmail.com>
2019-12-28 11:25:06 -0500

    name_generator_1

--- config/initializers/name_generator.rb
+++ config/initializers/name_generator.rb
@@ -3,8 +3,8 @@ $name_generator = RandomNameGenerator.new
 def name_generator
   [
     Forgery::Basic.color,
-    # gets 2 syllable fantasy name
-    $name_generator.compose(2),
+    # gets landmark or other item found in nature
+    Bazaar.super_item,
     # gets 4 random characters from 64 bit hash, joins them, and removes all "_" and "-"
     SecureRandom.urlsafe_base64.split('').sample(4).join.gsub(/(-|_)/, '')
     # joins all 3 together and downcases
