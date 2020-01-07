3cc46ae5a276788ea6260e9e6ab523e5be2bd1b5
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-01-07 12:04:10 -0500

    migration_fix_for_pg

--- db/migrate/20200107153431_rename_shared_item_max_borrow_duration_to_days_to_borrow.rb
+++ db/migrate/20200107153431_rename_shared_item_max_borrow_duration_to_days_to_borrow.rb
@@ -1,6 +1,6 @@
 class RenameSharedItemMaxBorrowDurationToDaysToBorrow < ActiveRecord::Migration[6.0]
   def change
     rename_column :shared_items, :max_borrow_duration, :days_to_borrow
-    change_column :shared_items, :days_to_borrow, :integer
+    change_column :shared_items, :days_to_borrow, :integer, "USING days_to_borrow::integer"
   end
 end
