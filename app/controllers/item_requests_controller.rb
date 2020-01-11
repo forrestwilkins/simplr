a0b7ca49e293c73e31040f54c84a4b9935e834b8
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-01-11 17:48:56 -0500

    user_profile_card_vertical_spacer

--- app/controllers/item_requests_controller.rb
+++ app/controllers/item_requests_controller.rb
@@ -12,11 +12,15 @@ class ItemRequestsController < ApplicationController
     if @item_request.save
       @shared_item = @item_request.shared_item
       @requester = @item_request.user
-      # send email to owner of item that someone is requesting to borrow/use
-      UserMailer.item_request(@item_request).deliver unless in_dev?
-      # sends sms text message to owner of the item
-      send_twilio_sms @shared_item.user.phone_number,
-        "#{@requester.name.capitalize} would like to borrow your #{@shared_item.name}, contact them to coordinate pickup."
+      unless in_dev?
+        # send email to owner of item that someone is requesting to borrow/use
+        UserMailer.item_request(@item_request).deliver if @shared_item.user.email.present?
+        # sends sms text message to owner of the item
+        if @shared_item.user.phone_number.present?
+          send_twilio_sms @shared_item.user.phone_number,
+            "#{@requester.name.capitalize} would like to borrow your #{@shared_item.name}, contact them to coordinate pickup."
+        end
+      end
       # notifies owner of the item through the sites notification system
       Note.notify :shared_item_request, @shared_item, @shared_item.user, current_user unless @shared_item.user.eql? current_user
     end
