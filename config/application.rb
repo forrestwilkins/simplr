f43c6cf37d758f9f084d848f730c498c79d6d807
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-01-11 12:29:34 -0500

    email_fix?

--- config/application.rb
+++ config/application.rb
@@ -30,13 +30,11 @@ module Simplr
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
-    if ENV['RAILS_ENV'].eql? 'production'
-      config.before_configuration do
-        env_file = File.join(Rails.root, 'config', 'local_env.yml')
-        YAML.load(File.open(env_file)).each do |key, value|
-          ENV[key.to_s] = value.to_s
-        end if File.exists?(env_file)
-      end
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value.to_s
+      end if File.exists?(env_file)
     end
   end
 end
