9ad5e44d6dff2dbb6c40e59210a8812abe267401
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-01-11 11:38:36 -0500

    email_auth_fix?

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: "example@gmail.com"
+  default from: "#{ENV["GMAIL_USERNAME"]}@gmail.com"
 
   def item_request(request)
     @user = request.shared_item.user
