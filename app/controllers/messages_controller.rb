9b7e4e66a9b5abce3e828a7e9619a52899c85d2a
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-01-19 11:43:54 -0500

    skip_before_action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   before_action :set_message, only: [:show]
   before_action :invite_only
 
-  skip_before_filter :verify_authenticity_token, only:  :twilio_reply
+  skip_before_action :verify_authenticity_token, only:  :twilio_reply
 
   def twilio_reply
     message_body = params["Body"]
