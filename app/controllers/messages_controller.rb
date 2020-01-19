9813cfc15100674045f75a59b85d48a95216d339
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-01-19 12:25:17 -0500

    twilio_reply1111

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,7 @@ class MessagesController < ApplicationController
 
   def twilio_reply
     message_body = params["Body"]
-    from_number = params["From"].gsub("+1", "") # makes number valid
+    from_number = params["From"]
     send_twilio_sms "Hello there, thanks for texting me. Your number is #{from_number}.", from_number
   end
 
