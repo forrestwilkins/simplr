7522d1cfb21a94f9fd229c7b4068290a3ea369a3
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-02-19 16:19:14 -0500

    fwc_main_img_lg

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,5 +1,13 @@
 class TemplatesController < ApplicationController
+  before_action :set_fwc
+  
   def co
+    @fwc_home = true
+  end
+  
+  private
+  
+  def set_fwc
     @forrest_web_co = true
   end
 end
