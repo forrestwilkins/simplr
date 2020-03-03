fdc06753e3fe46cb1aac3c14a6e732792f6da13b
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-03-03 10:00:35 -0500

    favicon

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,5 +1,7 @@
 class TemplatesController < ApplicationController
+  before_action :fw_only
   before_action :set_fwc
+
   layout 'co'
 
   def co
@@ -10,5 +12,11 @@ class TemplatesController < ApplicationController
 
   def set_fwc
     @forrest_web_co = true
+  end
+
+  def fw_only
+    unless forrest_web_co? or forrest_wilkins? or in_dev?
+      redirect_to '/404'
+    end
   end
 end
