84958e62c06a23d6ca6071290e6ac26325935e44
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-03-16 09:58:14 -0400

    lil_c_nav_menu_links

--- app/helpers/templates_helper.rb
+++ app/helpers/templates_helper.rb
@@ -1,2 +1,12 @@
 module TemplatesHelper
+  def get_lil_c_link
+    {
+      facebook: "https://www.facebook.com/LilCCorleone",
+      instagram: "https://www.instagram.com/cico_suave",
+      apple: "https://itunes.apple.com/us/album/its-different-this-time/1437074233",
+      youtube: "https://www.youtube.com/channel/UCvR3Mmrs3qUl4bQGfS9vTBw",
+      twitter: "https://twitter.com/lilccorleone",
+      spotify: "https://open.spotify.com/album/3zL9tBFintRc7vi8qdkfbv"
+    }
+  end
 end
